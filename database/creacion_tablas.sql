2634c6da3534eff80898f4b2b4058f61b2f45169
unknown <juan.andres@melgarejo.cl>
2023-05-14 23:39:42 -0400

    todas las tablas para sprint 1

--- database/creacion_tablas.sql
+++ database/creacion_tablas.sql
@@ -1,6 +1,14 @@
 -- gestion_flota.login definition
 -- gestion_flota.perfil definition
 
+drop TABLE if exists `solicitud`;
+drop TABLE if exists `carga`;
+drop TABLE if exists `comuna`;
+drop TABLE if exists `provincia`;
+drop TABLE if exists `region`;
+drop TABLE if exists `direccion`;
+drop TABLE if exists `cliente`;
+drop TABLE if exists `camion`;
 drop TABLE if exists `vehiculo`;
 drop TABLE if exists `planificacion`;
 drop TABLE if exists `conductor`;
@@ -56,18 +64,6 @@ CREATE TABLE if not exists `conductor` (
   PRIMARY KEY (`id_conductor`)  
 );
 
-CREATE TABLE if not exists `planificacion` (
-  `id_planificacion` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `conductor_id` int(11) REFERENCES empleado(id_empleado),
-  `fecha_creacion` datetime,
-  `fecha_planificacion` datetime,
-  `hora_salida` time,
-  `hora_retorno` time,
-  `estado` varchar(50),
-  `origen` varchar(100),
-  `destino` varchar(100),
-  PRIMARY KEY (`id_planificacion`)  
-);
 
 CREATE TABLE if not exists `vehiculo` (
   `id_vehiculo` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -86,4 +82,82 @@ CREATE TABLE if not exists `vehiculo` (
   `tipo` varchar(10),
   `estado` varchar(10),
   PRIMARY KEY (`id_vehiculo`)  
+);
+
+CREATE TABLE if not exists `camion` (
+  `id_camion` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `vehiculo_id` int(11) REFERENCES vehiculo(id_vehiculo),
+  `capacidad` float(9,2),
+  `tipo` varchar(30),
+  PRIMARY KEY (`id_camion`)  
+);
+
+
+CREATE TABLE if not exists `planificacion` (
+  `id_planificacion` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `conductor_id` int(11) REFERENCES empleado(id_empleado),
+  `camion_id` int(11) REFERENCES camion(id_camion),
+  `fecha_creacion` datetime,
+  `fecha_planificacion` datetime,
+  `hora_salida` time,
+  `hora_retorno` time,
+  `estado` varchar(50),
+  `origen` varchar(100),
+  `destino` varchar(100),
+  PRIMARY KEY (`id_planificacion`)  
+);
+
+
+CREATE TABLE if not exists `region` (
+  `id_region` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_region` varchar(100),
+  PRIMARY KEY (`id_region`)  
+);
+CREATE TABLE if not exists `provincia` (
+  `id_provincia` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_provincia` varchar(100),
+  PRIMARY KEY (`id_provincia`)  
+);
+CREATE TABLE if not exists `comuna` (
+  `id_comuna` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_comuna` varchar(100),
+  PRIMARY KEY (`id_comuna`)  
+);
+CREATE TABLE if not exists `direccion` (
+  `id_direccion` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `calle_numero` varchar(100),
+  `adicional` varchar(100),
+  `region_id` int(10),
+  `provincia_id` int(10),
+  `comuna_id` int(10),
+  PRIMARY KEY (`id_direccion`)  
+);
+CREATE TABLE if not exists `cliente` (
+  `id_cliente` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `persona_id` int(11) REFERENCES persona(id_persona),
+  `razon_social` varchar(100),
+  `direccion_id` int(10) REFERENCES direccion(id_direccion),
+  PRIMARY KEY (`id_cliente`)  
+);
+
+CREATE TABLE if not exists `solicitud` (
+  `id_solicitud` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cliente_id` int(11) REFERENCES cliente(id_cliente),
+  `planificacion_id` int(11) null REFERENCES planificacion(planificacion_id),
+  `estado` varchar(50),
+  `origen` varchar(100),
+  `destino` varchar(100),
+  `fecha_solicitud` datetime,
+  `fecha_entrega` datetime,
+  PRIMARY KEY (`id_solicitud`)  
+);
+
+CREATE TABLE if not exists `carga` (
+  `id_carga` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `solicitud_id` int(11) REFERENCES cliente(id_solicitud),
+  `tipo` varchar(50),
+  `peso` float(11,2),
+  `dimension` varchar(100),
+  `detalle` varchar(200),
+  PRIMARY KEY (`id_carga`)  
 );
