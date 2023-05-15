13d0afdf898d00dbd209f0c92c9a653b253f10d2
Felipe <admin@domain.com>
2023-05-15 12:54:00 -0400

    Initial commit

---
+++ backend/gestion_flota_backend/src/test/java/com/restfull/core/GestionFlotaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfull.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFlotaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
