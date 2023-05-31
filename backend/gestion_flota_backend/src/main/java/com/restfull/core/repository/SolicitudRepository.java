8c17d0f91d6ce6b5c2bb4338a8a866d1d504baa8
Juan Andres Melgarejo <juan.andres@melgarejo.cl>
2023-05-30 21:00:50 -0400

    actualizacion de solicitud controller

--- backend/gestion_flota_backend/src/main/java/com/restfull/core/repository/SolicitudRepository.java
+++ backend/gestion_flota_backend/src/main/java/com/restfull/core/repository/SolicitudRepository.java
@@ -19,12 +19,19 @@ public class SolicitudRepository {
 
 
 	public List<Solicitud> getAllSolicitud() {
-		String sql = "select idSolicitud, estado\r\n"
+		String sql = "select id_solicitud,cliente_id, planificacion_id, estado, origen \r\n"
+				+ ", destino, fecha_solicitud, fecha_entrega\r\n"
 				+ "from solicitud c2 ;";
 		List<Solicitud> solicitudList = new ArrayList<Solicitud>();
 		List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
 		rows.forEach(row -> {
 			Solicitud solicitud = new Solicitud();
+			Long id_solicitud = ((Number)row.get("id_solicitud")).longValue();
+			Long cliente_id = ((Number)row.get("cliente_id")).longValue();
+			Long planificacion_id = ((Number)row.get("planificacion_id")).longValue();
+			solicitud.setIdSolicitud(id_solicitud.intValue());
+			solicitud.setClienteId(cliente_id.intValue());
+			solicitud.setPlanificacionId(planificacion_id.intValue());
 			solicitud.setEstado((String)row.get("estado"));
 			solicitudList.add(solicitud);
 		});
