750b558759fe1e34e97b36d1a18c76542dd42a9d
tjimenezm <135542666+tjimenezm@users.noreply.github.com>
2023-06-04 18:05:38 -0400

    Update RegistrarSalidaController.java
    Elimina libreria que no se utiliza

--- backend/gestion_flota_backend/src/main/java/com/restfull/core/controllers/RegistrarSalidaController.java
+++ backend/gestion_flota_backend/src/main/java/com/restfull/core/controllers/RegistrarSalidaController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
+/*import org.springframework.web.bind.annotation.CrossOrigin;*/
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,41 +23,39 @@ import com.restfull.core.services.IRegistrarSalidaService;
 public class RegistrarSalidaController {
 	@Autowired
 	private IRegistrarSalidaService registrarSalidaService;
-	
+
 	@GetMapping("/registrarsalida")
-	public List<RegistrarSalida> index(){
+	public List<RegistrarSalida> index() {
 		return registrarSalidaService.findAll();
-	}	
-	
+	}
+
 	@GetMapping("/registrarsalida/{id}")
 	public RegistrarSalida show(@PathVariable Long id) {
 		return registrarSalidaService.findById(id);
 
 	}
-	
+
 	@PostMapping("/registrarsalida")
 	@ResponseStatus(HttpStatus.CREATED)
-	public RegistrarSalida create(@RequestBody RegistrarSalida registrarSalida){
+	public RegistrarSalida create(@RequestBody RegistrarSalida registrarSalida) {
 		return registrarSalidaService.save(registrarSalida);
 	}
-	
+
 	@PutMapping("/registrarsalida/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public RegistrarSalida update(@RequestBody RegistrarSalida registrarSalida, @PathVariable long id) {
-		
+
 		RegistrarSalida registrarSalidaActual = registrarSalidaService.findById(id);
-		
-		
+
 		registrarSalidaActual.setSalida(registrarSalida.getSalida());
 		registrarSalidaActual.setPatente(registrarSalida.getPatente());
 		registrarSalidaActual.setEstado(registrarSalida.getEstado());
 		return registrarSalidaService.save(registrarSalidaActual);
 	}
-	
+
 	@DeleteMapping("/registrarsalida/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		registrarSalidaService.delete(id);
 	}
 }
-
